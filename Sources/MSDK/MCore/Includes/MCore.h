06604b6cb90ba6fa6a295e543128955895ff87e6
David Elahee <david.elahee@gmail.com>
2018-11-09 12:17:04 +0100

    add some warning removal

--- Sources/MSDK/MCore/Includes/MCore.h
+++ Sources/MSDK/MCore/Includes/MCore.h
@@ -35,6 +35,11 @@
 
 	#ifdef _MSC_VER
 		#pragma warning(disable: 4251)
+		#pragma warning(disable: 4100)
+		#pragma warning(disable: 4189)
+		#pragma warning(disable: 4244)
+		#pragma warning(disable: 4267)
+		
 	#endif
 
 	// M_CORE_EXPORT
--- Sources/MSDK/MEngine/Includes/MEngine.h
+++ Sources/MSDK/MEngine/Includes/MEngine.h
@@ -35,6 +35,10 @@
 
 	#ifdef _MSC_VER
 		#pragma warning(disable: 4251)
+		#pragma warning(disable: 4100)
+		#pragma warning(disable: 4244)
+		#pragma warning(disable: 4267)
+		#pragma warning(disable: 4083)
 	#endif
 
 	// M_ENGINE_EXPORT
